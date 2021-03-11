b30db95b62d590233d58d6a3103a61a5b0068b7d
Jonathan-Majors <izzymajors@gmail.com>
2021-03-11 11:46:18 -0600

    wip

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+console.log('web 39 rules')
+
+const express = require('express')
+
+
+const server = express()
+
+server.use(express.json())
+
+console.log(process.env.NODE_ENV)
+
+if(process.env.NODE_ENV !== 'development'){ // on Heroku machin, an env variable is called "NODE_ENV" -> "production"
+     const cors = require('cors')
+    server.use(cors())
+}
+
+const PORT = process.env.PORT || 4000
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
