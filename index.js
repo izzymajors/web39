4945f78477636baf01ab594c225cc8ccc24428f9
Jonathan-Majors <izzymajors@gmail.com>
2021-03-11 12:31:07 -0600

    committing

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
+const path = require('path')
 console.log('web 39 rules')
 
 const express = require('express')
 
 
+
 const server = express()
 
 server.use(express.json())
+server.use(express.static( path.join(__dirname, 'client/build') ))
 
 console.log(process.env.NODE_ENV)
 
@@ -15,8 +18,12 @@ if(process.env.NODE_ENV !== 'development'){ // on Heroku machin, an env variable
     server.use(cors())
 }
 
-server.use('*', (req, res) => {
-    res.send('<h1>success</h1>')
+server.get('/api/hello', (req, res) => {
+    res.json({message: 'hello'})
+})
+
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))    
 })
 
 const PORT = process.env.PORT || 4000
