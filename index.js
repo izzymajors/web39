b514920b5adae495156b4b3b69713c4360de3ad8
Jonathan-Majors <izzymajors@gmail.com>
2021-03-11 13:11:02 -0600

    finished guided project

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 require('dotenv').config()
 const path = require('path')
-console.log('web 39 rules')
-
 const express = require('express')
-
-
 
 const server = express()
 
 server.use(express.json())
-server.use(express.static( path.join(__dirname, 'client/build') ))
+server.use(express.static(path.join(__dirname, 'client/build')))
 
 console.log(process.env.NODE_ENV)
 
-if(process.env.NODE_ENV !== 'development'){ // on Heroku machin, an env variable is called "NODE_ENV" -> "production"
-     const cors = require('cors')
-    server.use(cors())
+// on Heroku machine, an env variable is called "NODE_ENV" -> "production"
+if (process.env.NODE_ENV === 'development') {
+  const cors = require('cors')
+  server.use(cors())
 }
 
+// our API comes earlier in the pipeline
 server.get('/api/hello', (req, res) => {
-    res.json({message: 'hello'})
+  res.json({ message: 'i love you all' })
 })
 
+// catch-all that just sends back index.html
 server.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))    
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 const PORT = process.env.PORT || 4000
 
 server.listen(PORT, () => {
-    console.log(`listening on ${PORT}`)
+  console.log(`listening on ${PORT}`)
 })
